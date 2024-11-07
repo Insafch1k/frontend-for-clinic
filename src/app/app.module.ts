eb1e3c420573fea2b697d7e65df7ae6203124405
Insafch1k <ainsaf833@gmail.com>
2024-11-07 19:37:08 +0300

    создание

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './website/shared/pages/main/main.component';
+import { HeaderComponent } from './website/shared/layout/header/header.component';
+import { AboutModule } from './website/modules/about/about.module';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AboutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
