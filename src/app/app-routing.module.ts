6aec7753db170069ceac34ee243d3cd1f289fefb
Gosman Ganeev <usmangan43@gmail.com>
2024-11-10 16:42:08 +0300

    тезеттем header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,32 +3,40 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './website/shared/pages/main/main.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: MainComponent,
-    title: 'main',
-},
-{
-  path: 'about',
-  loadChildren: () =>
-      import('./website/modules/about/about.module').then(
-          (m) => m.AboutModule
-      ),
-  title: 'about',
-},
-{
-  path: '',
-  loadChildren: () =>
-      import('./website/modules/about/about.module').then(
-          (m) => m.AboutModule
-      ),
-  title: 'about',
-},
-{
-  path: '**',
-  component: MainComponent,
-  redirectTo: '',
-},
+    {
+        path: '',
+        component: MainComponent,
+        title: 'main',
+    },
+    {
+        path: 'about',
+        loadChildren: () =>
+            import('./website/modules/about/about.module').then(
+                (m) => m.AboutModule
+            ),
+        title: 'about',
+    },
+    {
+        path: 'analyzes',
+        loadChildren: () =>
+            import('./website/modules/analyzes/analyzes.module').then(
+                (m) => m.AnalyzesModule
+            ),
+        title: 'analyzes',
+    },
+    {
+        path: 'specialists',
+        loadChildren: () =>
+            import('./website/modules/specialists/specialists.module').then(
+                (m) => m.SpecialistsModule
+            ),
+        title: 'specialists',
+    },
+    {
+        path: '**',
+        component: MainComponent,
+        redirectTo: '',
+    },
 ];
 
 @NgModule({
