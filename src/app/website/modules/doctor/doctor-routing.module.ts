59f2880bde0d192442be806efaddd88f5829766f
Insafch1k <ainsaf833@gmail.com>
2025-01-10 16:28:10 +0300

    Специалисты, керттем окно выбор времени

--- src/app/website/modules/doctor/doctor-routing.module.ts
+++ src/app/website/modules/doctor/doctor-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DoctorComponent } from './doctor/doctor.component';
+import { AppointmentComponent } from './appointment/appointment.component';
 
 const routes: Routes = [
     {
         path: '',
         component: DoctorComponent,
     },
+    {
+        path:'zap',
+        component:AppointmentComponent
+    }
 ];
 
 @NgModule({
