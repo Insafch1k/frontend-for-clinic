069b65d579883cde7538c91de73179ffdc80622d
Insafch1k <ainsaf833@gmail.com>
2025-02-20 14:58:07 +0300

    Сообщение коммита, описывающее ваши изменения

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './website/shared/shared.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './website/admin/interceptors/http-interceptor';
 
 
 
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-    providers: [],
+    providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
